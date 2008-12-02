b0167e7b6a754c6f199495802183386865d3e78a
maddox <jon@jonsthoughtsoneverything.com>
2008-12-02 10:12:32 -0700

    Dependencies went to ActiveSupport::Dependencies in Rails 2.2

--- rails/init.rb
+++ rails/init.rb
@@ -6,8 +6,8 @@ require 'models/vote.rb'
 %w{ models controllers helpers }.each do |dir|
   path = File.join(File.dirname(__FILE__) , 'lib', dir)
   $LOAD_PATH << path
-  Dependencies.load_paths << path
-  Dependencies.load_once_paths.delete(path)
+  ActiveSupport::Dependencies.load_paths << path
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
 end
 
 ActiveRecord::Base.send(:include, Juixe::Acts::Voteable)
